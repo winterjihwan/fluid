06621f4b303febe9f26419146a3ed801bbf36338
Ji Hwan An <angeljihwan@gmail.com>
2025-02-03 14:50:11 +0900

    Textures

--- fluid.c
+++ fluid.c
@@ -2,6 +2,7 @@
 #include "SDL_events.h"
 #include "SDL_render.h"
 #include <stdio.h>
+#include <stdlib.h>
 
 static cell WORLD[CELL_COUNT_H * CELL_COUNT_W] = {0};
 
@@ -36,31 +37,50 @@ static void fluid_render_world(SDL_Renderer *renderer) {
 }
 
 static void fluid_cell_update(SDL_Renderer *renderer, int x, int y,
-                              color texture) {
+                              color texture, int erase) {
   x = x / CELL_SIZE;
   y = y / CELL_SIZE;
   WORLD[y * CELL_COUNT_W + x].texture = texture;
-  WORLD[y * CELL_COUNT_W + x].none = 0;
+  WORLD[y * CELL_COUNT_W + x].none = erase;
 }
 
 void fluid_event_handle(SDL_Renderer *renderer, SDL_Event *e) {
-  static int d, t = 0;
+  static int d, erase = 0;
+  static int type = 1;
   if (e->type == SDL_MOUSEBUTTONDOWN)
     d = 1;
   if (e->type == SDL_MOUSEMOTION && d) {
     color c;
-    switch (t) {
-    case 0:
-      c = TEX_BUCKET;
-      break;
+    switch (type) {
     case 1:
-      c = TEX_WATER;
+      c = TEX_BUCKET;
+      erase = 0;
       break;
+    case 2:
+      c = TEX_WATER;
+      erase = 0;
+      break;
+    case 3:
+      c = TEX_NONE;
+      erase = 1;
+      break;
+    default:
+      fprintf(stdout, "Unknown Texture");
+      exit(1);
     }
-    fluid_cell_update(renderer, e->motion.x, e->motion.y, TEX_BUCKET);
+    fluid_cell_update(renderer, e->motion.x, e->motion.y, c, erase);
   }
   if (e->type == SDL_MOUSEBUTTONUP)
     d = 0;
+  if (e->type == SDL_KEYDOWN) {
+    if (e->key.keysym.scancode == SDL_SCANCODE_1) {
+      type = 1;
+    } else if (e->key.keysym.scancode == SDL_SCANCODE_2) {
+      type = 2;
+    } else if (e->key.keysym.scancode == SDL_SCANCODE_3) {
+      type = 3;
+    }
+  }
 }
 
 void fluid_initialize_static(SDL_Renderer *renderer) {
